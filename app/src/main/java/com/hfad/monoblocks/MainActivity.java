f6994eff873881fd4a79e1279046b1aab41dd1f9
etogus <mamedov-guseyn@mail.ru>
2022-03-28 23:27:01 +0300

    Initial commit

---
+++ app/src/main/java/com/hfad/monoblocks/MainActivity.java
@@ -0,0 +1,50 @@
+package com.hfad.monoblocks;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
+
+import io.realm.Realm;
+import io.realm.RealmChangeListener;
+import io.realm.RealmResults;
+import io.realm.Sort;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MaterialButton addMonoButton = findViewById(R.id.addnewmonobutton);
+
+        addMonoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AddMonoActivity.class));
+            }
+        });
+        Realm.init(getApplicationContext());
+        Realm realm = Realm.getDefaultInstance();
+
+        RealmResults<Mono> monoList = realm.where(Mono.class).findAllSorted("createdTime", Sort.DESCENDING);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        MyAdapter myAdapter = new MyAdapter(getApplicationContext(),monoList);
+        recyclerView.setAdapter(myAdapter);
+
+        monoList.addChangeListener(new RealmChangeListener<RealmResults<Mono>>() {
+            @Override
+            public void onChange(RealmResults<Mono> monos) {
+                myAdapter.notifyDataSetChanged();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/hfad/monoblocks/MyAdapter.java
@@ -0,0 +1,97 @@
+package com.hfad.monoblocks;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    Context context;
+    RealmResults<Mono> monoList;
+
+    public MyAdapter(Context context, RealmResults<Mono> monoList) {
+        this.context = context;
+        this.monoList = monoList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MyViewHolder(LayoutInflater.from(context).inflate(R.layout.item_view, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyAdapter.MyViewHolder holder, int position) {
+        Mono mono = monoList.get(position);
+        holder.name.setText(mono.getName());
+        String companyText = mono.getCompany() + ", ";
+        holder.company.setText(companyText);
+        String screenText = mono.getScreen() + "\", ";
+        holder.screen.setText(screenText);
+        String ramText = mono.getRam() + "ГБ ОЗУ, ";
+        holder.ram.setText(ramText);
+        String ssdText = mono.getSsd() + "ГБ SSD, ";
+        holder.ssd.setText(ssdText);
+        holder.system.setText(mono.getSystem());
+
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+
+                PopupMenu menu = new PopupMenu(context,v);
+                menu.getMenu().add("Удалить");
+                menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        if(item.getTitle().equals("Удалить")){
+                            Realm realm = Realm.getDefaultInstance();
+                            realm.beginTransaction();
+                            mono.deleteFromRealm();
+                            realm.commitTransaction();
+                            Toast.makeText(context,"Удалено",Toast.LENGTH_SHORT).show();
+                        }
+                        return true;
+                    }
+                });
+                menu.show();
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return monoList.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView name;
+        TextView company;
+        TextView screen;
+        TextView ram;
+        TextView ssd;
+        TextView system;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.name);
+            company = itemView.findViewById(R.id.company);
+            screen = itemView.findViewById(R.id.screen);
+            ram = itemView.findViewById(R.id.ram);
+            ssd = itemView.findViewById(R.id.ssd);
+            system = itemView.findViewById(R.id.system);
+        }
+    }
+}
